d9f6356e8e9ac29f2e1e4d0b9c03208beb9a0d08
holic <3485995896@qq.com>
2024-08-13 15:19:02 +0800

    solved problem4

--- asst1/prog4_sqrt/data.cpp
+++ asst1/prog4_sqrt/data.cpp
@@ -14,7 +14,7 @@ void initGood(float *values, int N) {
     for (int i=0; i<N; i++)
     {
         // Todo: Choose values
-        values[i] = 1.0f;
+        values[i] = 2.999f;
     }
 }
 
@@ -22,8 +22,38 @@ void initGood(float *values, int N) {
 void initBad(float *values, int N) {
     for (int i=0; i<N; i++)
     {
-        // Todo: Choose values
         values[i] = 1.0f;
+
+        //
+        // min speedup from ISPC
+        //
+
+        //if(i % 8 == 0) values[i] = 2.999f;  
+
+        /*
+
+            [sqrt serial]:          [483.695] ms
+            [sqrt ispc]:            [518.981] ms
+            [sqrt task ispc]:       [18.148] ms
+                                            (0.93x speedup from ISPC)
+                                            (26.65x speedup from task ISPC)
+        */
     }
+
+    //
+    // min speedup from task ISPC
+    //
+
+    for(int i = 0; i < N / 64; i ++) {
+        if(i % 8 == 0) values[i] = 2.999f;
+    }
+
+    /*
+        [sqrt serial]:          [36.856] ms
+        [sqrt ispc]:            [24.352] ms
+        [sqrt task ispc]:       [11.724] ms
+                                        (1.51x speedup from ISPC)
+                                        (3.14x speedup from task ISPC)
+    */
 }
 
